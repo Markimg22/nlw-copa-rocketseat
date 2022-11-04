eeb5dbf276b84e237bacb33d78c9661c6ccb6473
Markimg22 <marquim.net.221200@hotmail.com>
2022-11-04 16:29:45 -0400

    aula-1: setup mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,21 @@
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { THEME } from './src/styles/theme'
+import { Loading } from './src/components'
+import { SignIn } from './src/screens'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
